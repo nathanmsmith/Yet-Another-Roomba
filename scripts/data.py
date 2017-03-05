0e1d67dd857cdb8b2ac9cdcb4e30714b7729ef28
Nathan Smith <nathan@nathansmith.io>
2017-03-04 19:36:32 -0800

    Start to read data script

--- scripts/data.py
+++ scripts/data.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import create2api
+
+bot = create2api.Create2()
+
 bot.get_packet(100)
-print "angle: ", bot.sensor_state["angle"]
-print "distance: ", bot.sensor_state["distance"]
+print("Angle: " + str(bot.sensor_state["angle"]))
+print("Distance: " + str(bot.sensor_state["distance"]))
